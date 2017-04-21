393f1708600c907138da9185e8ed8f9546f73991
joseportillo <jiprizzo@gmail.com>
2017-04-21 13:01:19 +0200

    Jose 30 min sprint - view

--- lib/view.rb
+++ lib/view.rb
@@ -1,12 +1,7 @@
 # view
-
 class View
 
 
-
-
-  # OUTPUT
-  # List all recipes
 
 
   # INPUT
@@ -15,8 +10,24 @@ class View
   # Exit program
 
 
+#create
+
+# def ask_recipe_name
+#  puts "What is the name of your new recipe?"
+#  print ">"
+#  return = gets.chomp
+# end
+
+  def define_description
+    puts "Insert description"
+    print ">"
+  return = gets.chomp
+  end
 
 
-
-
+  # OUTPUT
+  def display(repo)
+  repo.each_with_index do |recipe, index|
+    puts "#{index+1}: #{recipe} "
+  end
 end
